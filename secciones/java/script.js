afe4e7a7a166efa7f347dd61547cfe2e82430d6c
Javier Perez <jpb0029@alu.medac.es>
2025-04-07 10:21:51 +0200

    Muchas cosa 2

--- secciones/java/script.js
+++ secciones/java/script.js
@@ -6,14 +6,19 @@ function checkAnswer(isCorrect, gifUrl) {
 
     // Mostrar el mensaje de respuesta
     const resultElement = document.getElementById("result");
+    const nextQuestionContainer = document.getElementById("nextQuestionContainer");
+
     if (isCorrect) {
         resultElement.innerHTML = "¡Correcto! El código está bien.";
         resultElement.className = "result correct";  // Cambia el estilo a correcto
+        nextQuestionContainer.style.display = "block";  // Muestra el botón de siguiente pregunta
     } else {
         resultElement.innerHTML = "¡Incorrecto! Intenta nuevamente.";
         resultElement.className = "result incorrect";  // Cambia el estilo a incorrecto
+        nextQuestionContainer.style.display = "none";  // Oculta el botón de siguiente pregunta si la respuesta es incorrecta
     }
 }
+
 
 
 
